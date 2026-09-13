a63d72f4343527e122f21a7e8f1c954ad205f806
Amit Shaw <amitwork708090@gmail.com>
2026-09-13 20:50:13 +0530

    Time: 48 ms (74.43%), Space: 51.8 MB (70.87%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,21 +1,26 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int targetFriendArrival = times[targetFriend][0];
+        
+        // [[1,4],[2,3],[4,6]], targetFriend = 1
+
+        int targetFriendArrival = times[targetFriend][0]; // 2
 
         Arrays.sort(times, (a, b) -> a[0] - b[0]);
+        // [[1,4],[2,3],[4,6]]
 
         PriorityQueue<Integer> avaliableChairs = new PriorityQueue<>();
 
         PriorityQueue<int[]> occupiedChairs = new PriorityQueue<>((a, b) -> a[0] - b[0]); // int[] {endTime, index}
 
         for(int i=0; i<times.length; i++) {
-            avaliableChairs.add(i);
+            avaliableChairs.add(i); // 0, 1, 2
         }
 
         for(int i=0; i<times.length; i++) {
-            int at = times[i][0];
+            int at = times[i][0]; // "at" means avaliableTime = 2
 
             while(!occupiedChairs.isEmpty()) {
+                    // 2 >= 4
                 if(at >= occupiedChairs.peek()[0]) {
                     int[] chair = occupiedChairs.poll();
                     avaliableChairs.add(chair[1]);
@@ -25,14 +30,15 @@ class Solution {
                 }
             }
 
-            int c = avaliableChairs.poll();
-
+            int c = avaliableChairs.poll(); // 0, 1, 2 -> 1, 2 -> 2
+            
+                // 2 == 2 return true
             if(at == targetFriendArrival) {
                 return c;
             }
 
             occupiedChairs.add(new int[]{
-                times[i][1], c // endTime, index
+                times[i][1], c // endTime, index oc = {4,0}
             });
 
 
